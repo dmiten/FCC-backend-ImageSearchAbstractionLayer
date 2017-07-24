7c7df7546b0a74c95e9d833d88e684c910d76733
netlan <netlan@yandex.ru>
2017-07-24 09:12:49 +0700

    Unused import in server.js removed.

--- app/server.js
+++ app/server.js
@@ -4,7 +4,6 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 
-import handleDb from "./handledb.js";
 import logic from "./logic.js";
 
 export default function server() {
